cadbb34f3e50daa46cf3d650ea10995366f32b8b
Saber <72188179+saberistalifi@users.noreply.github.com>
2020-10-01 19:13:06 +0800

    added

---
+++ addition.php
@@ -0,0 +1,105 @@
+<html>
+<head><h1 align="center"> Math Quiz </h1>
+	<link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+<body>
+<form action="Aditionoutput.php" method="post">
+<fieldset>
+<legend> Fill up </legend>
+<label> UserID </label>
+<input type="text" name="userid"/>
+&nbsp; &nbsp;
+<label> Name </label>
+<input type="text" name="name"/>
+&nbsp; &nbsp;
+<label> Age </label>
+<input type="text" name="age"/>
+
+
+
+</fieldset><br>
+	<header>
+		<div class="container">	</div>
+		</header>
+		<main>
+	<div class="container">
+
+<h2> Additional </h2>
+<p> Answer all the question </p>
+<table border="0" cellpadding="1">
+<tr>
+<label> (1). 12 + 13 = </label>
+<input type="text" name="qq1" id="q1" >
+</br></br>
+<label> (2). 18 + 12 = </label>
+<input type="text" name="qq2" >
+</br></br>
+<label> (3). 19 + 11 = </label>
+<input type="text" name="qq3" >
+</br></br>
+<label> (4). 22 + 22 = </label>
+<input type="text" name="qq4">
+</br></br>
+<label> (5). 21 + 29 = </label>
+<input type="text" name="qq5" >
+</br></br>
+<label> (6). 33 + 12 = </label>
+<input type="text" name="qq6" >
+</tr></br></br>
+
+<h4><b> Multiple Choice </h4></b>
+<p> Chose the correct answer! </p>
+<label>  (7). You have RM22 and your father will give you RM17. How much you will have in total? </label></br>
+<input type="radio" name="q7" value="38"  > A. 38 </br>
+<input type="radio" name="q7" value="37" > B. 37 </br>
+<input type="radio" name="q7" value="39"> C. 39 </br>
+<input type="radio" name="q7" value="38"> D. 40 </br>
+</br>
+<label> (8). I have RM50 and i borrow RM20, How much money do i have? </label></br>
+<input type="radio" name="q8" value="60" > A. 60 </br>
+<input type="radio" name="q8" value="70"> B. 70 </br>
+<input type="radio" name="q8" value="80" > C. 80 </br>
+<input type="radio" name="q8" value="50" > D. 50 </br>
+</br>
+<img src="RM50.jpg" width="80" hieght="20" />
+<img src="RM1.jpg" width="100" hieght="100" />
+<img src="RM5.jpg" width="100" hieght="100" />
+<img src="RM10.jpg" width="100" hieght="100" /></br>
+<label> (9). How much money? </label></br>
+<input type="radio" name="q9" value="16" > A. 16 </br>
+<input type="radio" name="q9" value="16.50" > B. 16.50 </br>
+<input type="radio" name="q9" value="16.40" > C. 16.40 </br>
+<input type="radio" name="q9" value="15.50" > D. 15.50</br>
+</br>
+<img src="car.png" width="100" hieght="100" />
+<img src="car.png" width="100" hieght="100" />
+<img src="car.png" width="100" hieght="100" />
+<img src="car.png" width="100" hieght="100" />
+<img src="car.png" width="100" hieght="100" />
+<img src="car.png" width="100" hieght="100" />
+<img src="car.png" width="100" hieght="100" />
+<img src="car.png" width="100" hieght="100" />
+<img src="car.png" width="100" hieght="100" />
+</br>
+<label> (10). How many car? </label></br>
+<input type="radio" name="q10" value="5" > A. 5 </br>
+<input type="radio" name="q10" value="8" > B. 8 </br>
+<input type="radio" name="q10" value="9" > C. 9 </br>
+<input type="radio" name="q10" value="10" > D. 10</br>
+</br>
+</table>
+<hr>
+<input type="submit" name="Submit">
+
+	</div>
+	</main>
+		<footer>
+			<div class="container">
+			Copyright &copy; 2020 Math Quiz
+			</div>
+		</footer>
+
+</form>
+</body>
+</html>
+
